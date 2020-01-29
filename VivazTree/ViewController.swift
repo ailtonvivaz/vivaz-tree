08e27a4d45a4c83302087eab861d1f966499ce1d
Ailton Vieira Pinto Filho <ailtonvivaz@outlook.com>
2020-01-29 16:14:02 -0300

    Sample

--- VivazTree/ViewController.swift
+++ VivazTree/ViewController.swift
@@ -6,15 +6,63 @@
 //  Copyright © 2020 Veevaz. All rights reserved.
 //
 
+import ARKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sceneView.delegate = self
+        configureLighting()
+        resetTrackingConfiguration()
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    func resetTrackingConfiguration() {
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
+        sceneView.session.run(configuration, options: options)
+//        label.text = "Move camera around to detect images"
+    }
 }
 
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        let referenceImage = imageAnchor.referenceImage
+        let imageName = referenceImage.name ?? "no name"
+        
+        let plane = SCNPlane(width: referenceImage.physicalSize.width, height: referenceImage.physicalSize.height)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.80
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        print(imageName)
+        
+//        planeNode.runAction(imageHighlightAction)
+        
+        node.addChildNode(planeNode)
+//        DispatchQueue.main.async {
+//            self.label.text = "Image detected: \"\(imageName)\""
+//        }
+    }
+}
